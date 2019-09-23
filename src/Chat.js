613e5b9e7d71f1840ce1404eb253cbabf64b2576
VladYem <volodymyem@gmail.com>
2019-09-23 22:54:12 +0200

    Server set up, exchange of messages

--- src/Chat.js
+++ src/Chat.js
@@ -2,11 +2,36 @@ import React, { Component } from 'react';
 import ChatInput from './ChatInput';
 import ChatMessage from './ChatMessage';
 
+const URL = 'ws://localhost:3030';
+
 class Chat extends Component {
   state = {
     name: 'Vlad',
-    message: []
+    messages: []
   };
+
+  ws = new WebSocket(URL);
+
+  componentDidMount() {
+    this.ws.onopen = () => {
+      // on connecting, do nothing but log it to the console
+      console.log('connected')
+    }
+
+    this.ws.onmessage = evt => {
+      // on receiving a message, add it to the list of messages
+      const message = JSON.parse(evt.data)
+      this.addMessage(message)
+    }
+
+    this.ws.onclose = () => {
+      console.log('disconnected')
+      // automatically try to reconnect on connection loss
+      this.setState({
+        ws: new WebSocket(URL),
+      })
+    }
+  }
 
   addMessage = message =>
     this.setState(state => ({ messages: [message, ...state.messages] }));
@@ -14,6 +39,7 @@ class Chat extends Component {
   SubmitMessage = messageString => {
     // on submitting the ChatInput form, send the message, add it to the list and reset the input
     const message = { name: this.state.name, message: messageString };
+    this.ws.send(JSON.stringify(message));
     this.addMessage(message);
   };
 
@@ -31,10 +57,20 @@ class Chat extends Component {
           />
         </label>
         <ChatInput
+          ws={this.ws}
           onSubmitMessage={messageString => this.SubmitMessage(messageString)}
         />
+        {this.state.messages.map((message, index) => {
+          return( 
+            <ChatMessage
+            key={index}
+            message={message.message}
+            name={message.name}
+          />
+          );
+        })}
 
-        <ChatMessage />
+      
       </div>
     );
   }
